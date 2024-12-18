97ff353118a0c56b9b0e2f647312ae1d1637333f
JoeyFaris <joeyfaris1@yahoo.com>
2024-12-18 13:30:59 -0800

    added lazy loading for canvas load

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,33 +1,39 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, lazy } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import { OrbitControls, Preload } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
-
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.6 : 0.65}
-        position={isMobile ? [0, -3, -2.2] : [0, -2.75, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  );
-};
+const Computers = lazy(() => {
+  return new Promise(resolve => {
+    import('@react-three/drei').then(module => {
+      const Component = ({ isMobile }) => {
+        const computer = module.useGLTF("/desktop_pc/scene.gltf");
+        
+        return (
+          <mesh>
+            <hemisphereLight intensity={0.15} groundColor='black' />
+            <spotLight
+              position={[-20, 50, 10]}
+              angle={0.12}
+              penumbra={1}
+              intensity={1}
+              castShadow
+              shadow-mapSize={1024}
+            />
+            <pointLight intensity={1} />
+            <primitive
+              object={computer.scene}
+              scale={isMobile ? 0.6 : 0.65}
+              position={isMobile ? [0, -3, -2.2] : [0, -2.75, -1.5]}
+              rotation={[-0.01, -0.2, -0.1]}
+            />
+          </mesh>
+        );
+      };
+      resolve(Component);
+    });
+  });
+});
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
